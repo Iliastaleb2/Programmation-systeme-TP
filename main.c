c36a1bd1bcb3560665d7d846b974a77894f456d3
IliasTaleb <ilias.taleb@ensea.fr>
2025-12-10 16:18:10 +0100

    new main

--- main.c
+++ main.c
@@ -2,5 +2,18 @@
 
 int main() {
     Welcome_prompt();
-    return EXIT_SUCCESS;
+    Normal_prompt();
+while(1){
+    char command_entered_by_user[128];  
+    Execution_of_a_simple_command(command_entered_by_user);
+    Normal_prompt();
+    if (feof(stdin)) {
+        Exit_prompt_or_ctrl_d_prompt();
 }
+
+
+    return EXIT_SUCCESS;
+
+}
+}
+
